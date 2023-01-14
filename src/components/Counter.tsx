e4f8cb515b3ab2901d2781b1ebf83df600bb3943
Shu25 <myoubann613@gmail.com>
2023-01-14 20:21:21 +0900

    change click num 500 to 1

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,7 +4,7 @@ import "./Counter.css";
 export default function Counter() {
   const [count, setCount] = createSignal(0);
   return (
-    <button class="increment" onClick={() => setCount(count() + 500)}>
+    <button class="increment" onClick={() => setCount(count() + 1)}>
       Clicks: {count()}
     </button>
   );
